2dd82856399d3edc3400877e8030a7a6fb72cd6b
Lukas Petersen <lukas.petersen@kit.edu>
2024-03-11 18:01:32 +0100

    justus python submission script cleanup

--- qpython_justus.sh
+++ qpython_justus.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-<<<<<<< HEAD
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=8G
 #SBATCH --time=12:00:00
 #SBATCH --output=train.out
 #SBATCH --error=train.err
+#SBATCH --open-mode=append
 #SBATCH --gres=gpu:1
 
 PYTHON_ENV=kgcnn_new
@@ -17,48 +17,6 @@ config_path="$2" # optional
 echo "# Date: " $(date)
 echo "# Hostname: " $SLURM_JOB_NODELIST
 echo "# Job ID: " $SLURM_JOB_ID
-=======
-#$ -l qu=gtx
-#$ -q gtx01a,gtx01b,gtx01c,gtx01d,gtx02a,gtx02b,gtx02c,gtx02d,gtx03a,gtx03b,gtx03c,gtx03d,gtx04a,gtx04b,gtx04c,gtx04d,gtx05a,gtx05b,gtx05c,gtx05d,gtx06a,gtx06b,gtx06c,gtx06d
-#$ -cwd
-#$ -o train.out
-#$ -e train.err 
-
-pythonfile=$1
-pythonfile=${pythonfile#"/srv/nfs"}
-
-config_path="$2" # optional
-
-# Which GPU?
-gpu_id=$( echo $QUEUE | awk '/a/ {print 0} /b/ {print 1}  /c/ {print 2}  /d/ {print 3}')
-
-# How many cores are there?
-case $HOSTNAME in
-    gtx0[1-6]*)
-    cores=10;
-    ;;
-    gtx0[7-8]*)
-    cores=12;
-    ;;
-    gtx09*)
-    cores=16;
-    ;;
-    gtx10*)
-    cores=16;
-    ;;
-    *)
-    echo "Error: Unknown compute node \$HOSTNAME"
-    echo "       This script only works for gtx01 thru 10!"
-    echo
-    exit -1
-    ;;
-esac
-
-# Echo important information into file
-echo "# Hostname: " `hostname`
-echo "# Job ID: " $JOB_ID
-echo "# gpuid: " $gpu_id
->>>>>>> 9ab32ee2eed1d0a12c9db1b531265e70c5846fdd
 
 # In case of external API usage I saved some API-keys here
 if [ -f ~/.api_keys ]; then
@@ -68,7 +26,6 @@ fi
 # For WandB:
 export WANDB_MODE=offline # no internet connection during calculation on nodes
 
-<<<<<<< HEAD
 CONDA_HOME=$(dirname $(dirname $CONDA_EXE))
 source $CONDA_HOME/etc/profile.d/conda.sh
 conda activate $PYTHON_ENV
@@ -81,43 +38,3 @@ then time python3 $pythonfile -g 0
 else time python3 $pythonfile -g 0 -c $config_path
 fi
 
-=======
-# For data readin in kgcnn
-export BABEL_DATADIR="/usr/local/run/openbabel-2.4.1"
-
-export PATH="/home/lpetersen/anaconda_interpreter/bin:$PATH"
-source /home/lpetersen/anaconda_interpreter/etc/profile.d/conda.sh
-conda activate kgcnn_new
-
-# Deprecated CUDA setting on server
-# export XLA_FLAGS="--xla_gpu_cuda_data_dir=/usr/lib/cuda"
-# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/run/cuda/lib
-
-# Even older deprecated CUDA setting on server
-#CUDNN_PATH=$(dirname $(python -c "import nvidia.cudnn;print(nvidia.cudnn.__file__)"))
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/:$CUDNN_PATH/lib
-
-# set OpenMP parallel threads variable:
-export OMP_NUM_THREADS=$cores
-
-# OpenMP needs this: set stack size to unlimited
-ulimit -s unlimited
-# Start time of calculation
-start=$( date "+%s" )
-
-if [ -z "$config_path" ]
-then time python3 $pythonfile -g $gpu_id
-else time python3 $pythonfile -g $gpu_id -c $config_path
-fi
-
-# End time of calculation
-end=$( date "+%s" )
-# Now we calculate the time taken by the calculation
-duration=$(( end - start ))
-# Now we do Wibbly Wobbly Timey Wimey... Stuff
-DAYS=$(( duration / 86400 ))
-HOURS=$(( (duration % 86400) / 3600 ))
-MINS=$(( ((duration % 86400) % 3600) / 60 ))
-SECS=$(( ((duration % 86400) % 3600) % 60 ))
-echo "Time taken: $DAYS days, $HOURS hours, $MINS minutes and $SECS seconds."
->>>>>>> 9ab32ee2eed1d0a12c9db1b531265e70c5846fdd
--- submit_python_file_justus.sh
+++ submit_python_file_justus.sh
@@ -1,8 +1,9 @@
 # Submit the python script to the queue and gives it the name of the current folder in the queue
 # Use the -s flag to keep this process running until it finishes and sync training data to wandb
 
-<<<<<<< HEAD
-python_script="/lustre/home/ka/ka_ipc/ka_he8978/kgcnn_fork/force_hdnnp4th.py"
+#python_script="/lustre/home/ka/ka_ipc/ka_he8978/kgcnn_fork/force_hdnnp4th.py"
+python_script="/lustre/home/ka/ka_ipc/ka_he8978/kgcnn_fork/force_hdnnp4th_hyp_param_search.py"
+
 queue_script="/lustre/home/ka/ka_ipc/ka_he8978/bin/qpython_justus.sh"
 # For data readin in kgcnn
 export BABEL_DATADIR="/opt/bwhpc/common/chem/openbabel/3.1.1"
@@ -28,35 +29,13 @@ while getopts ':c:s' flag; do
       echo "Option -$OPTARG requires an argument."
       print_usage
       exit 1;;
-=======
-#python_script="/home/lpetersen/dftb-nn/Behler_4th_Gen.py"
-#python_script="/home/lpetersen/kgcnn_fork/hdnnp4th.py"
-#python_script="/home/lpetersen/kgcnn_fork/force_hdnnp4th.py"
-#python_script="/home/lpetersen/kgcnn_fork/hdnnp2nd.py"
-python_script="/home/lpetersen/kgcnn_fork/force_hdnnp4th_hyp_param_search.py"
-#python_script="/home/lpetersen/kgcnn_fork/charge_hyp_param_search.py"
-#python_script="/home/lpetersen/kgcnn_fork/test_load_model.py"
-#python_script="/home/lpetersen/kgcnn_fork/retrieve_trial.py"
-
-queue_script="/home/lpetersen/bin/qpython.sh"
-
-print_usage() {
-  echo "Usage: 'submit_python_file.sh' to run without wandb or 'submit_python_file.sh -s' to sync to wandb"
-}
-
-sync=false
-while getopts 's' flag; do
-  case "${flag}" in
-    s) sync=true ;;
->>>>>>> 9ab32ee2eed1d0a12c9db1b531265e70c5846fdd
     *) print_usage
        exit 1 ;;
   esac
 done
 
-<<<<<<< HEAD
 if [ -z "$config_path" ]
-then echo "Did not get a config_file"
+then echo "INFO: Did not get a config_file"
 fi
 
 name=`basename $PWD`
@@ -70,24 +49,3 @@ echo `date`" $PWD" >> $HOME/checklist.txt
 # then
 #     nohup sync_wandb.sh $job_id &
 # fi
-=======
-# if [ -f train.err ]
-# then rm train.err
-# fi
-
-# if [ -f train.out ]
-# then rm train.out
-# fi
-
-name=`basename $PWD`
-job_id=$(qsub -terse -N $name $queue_script $python_script)
-echo "Submitted job $job_id to queue as $name"
-
-echo `date`" $PWD" >> /data/$USER/checklist.txt
-
-# for wandb sync
-if $sync
-then
-    nohup sync_wandb.sh $job_id &
-fi
->>>>>>> 9ab32ee2eed1d0a12c9db1b531265e70c5846fdd
