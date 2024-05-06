ae3ff22735f2402918302fd2ddde42c2f77a9bdc
Lukas Petersen <lukas.petersen@kit.edu>
2024-05-06 18:59:32 +0200

    chore: Refactor submission script to improve readability and add usage instructions

--- submit_python_file_justus.sh
+++ submit_python_file_justus.sh
@@ -1,10 +1,7 @@
 # Submit the python script to the queue and gives it the name of the current folder in the queue
 # Use the -s flag to keep this process running until it finishes and sync training data to wandb
-
-#python_script="/lustre/home/ka/ka_ipc/ka_he8978/kgcnn_fork/force_hdnnp4th.py"
-# python_script="/lustre/home/ka/ka_ipc/ka_he8978/kgcnn_fork/force_hdnnp4th_hyp_param_search.py"
-python_script="/home/ka/ka_ipc/ka_he8978/kgcnn_fork/retrieve_trial.py"
-# python_script=/home/ka/ka_ipc/ka_he8978/kgcnn_fork/calc_prediction_std.py
+# Use the -c flag to submit a python file with a configuration file
+# Use the -p flag to specify the python file to run
 
 queue_script="/lustre/home/ka/ka_ipc/ka_he8978/bin/qpython_justus.sh"
 # For data readin in kgcnn
@@ -12,6 +9,7 @@ export BABEL_DATADIR="/opt/bwhpc/common/chem/openbabel/3.1.1"
 
 print_usage() {
   echo "Usage: 'submit_python_file.sh' to run without wandb or 'submit_python_file.sh -s' to sync to wandb"
+  echo "Usage: 'submit_python_file.sh -p python_file' to submit a python file"
   echo "Usage: 'submit_python_file.sh -c config_path' to submit a python file with a configuration file"
 }
 
@@ -38,6 +36,13 @@ while getopts ':p:c:s' flag; do
        exit 1 ;;
   esac
 done
+
+if [ -z "$python_script" ]
+then
+  echo "ERROR: Python file not specified."
+  print_usage
+  exit 1
+fi
 
 if [ -z "$config_path" ]
 then echo "INFO: Did not get a config_file"
