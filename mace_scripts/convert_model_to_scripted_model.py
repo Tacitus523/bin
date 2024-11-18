51eba62cb72c63e45ad8cc5bf56677e208b7025f
Lukas Petersen <lukas.petersen@kit.edu>
2024-11-18 16:31:20 +0100

    feat: Enhance model selection logic in conversion script for better handling of multiple models

--- mace_scripts/convert_model_to_scripted_model.py
+++ mace_scripts/convert_model_to_scripted_model.py
@@ -23,10 +23,27 @@ def parse_args():
 def main():
     args = parse_args()
     model_paths = [f for f in os.listdir(os.getcwd()) if f.endswith('.model') and f.startswith(args.model_prefix)]
-    for model_path in model_paths:
-        model = torch.load(model_path)
-        model_compiled = jit.compile(deepcopy(model))
-        torch.jit.save(model_compiled, model_path.replace('.model', '.pt'))
+    model_paths = sorted(model_paths)
+    if len(model_paths) == 0:
+        print(f'No models found with prefix {args.model_prefix}')
+        return
+    elif len(model_paths) > 1:
+        print(f'Multiple models found with prefix {args.model_prefix}: {model_paths}')
+        swa_model_paths = [f for f in model_paths if 'swa' in f]
+        if len(swa_model_paths) == 0:
+            target_model_path = model_paths[-1]
+            print(f'No SWA models found, using the last model: {target_model_path}')
+        elif len(swa_model_paths) == 1:
+            target_model_path = swa_model_paths[0]
+            print(f'Using the SWA model: {target_model_path}')
+        else:
+            print(f'Multiple SWA models found: {swa_model_paths}')
+            target_model_path = swa_model_paths[-1]
+            print(f'Using the last SWA model: {target_model_path}')
+
+    model = torch.load(target_model_path)
+    model_compiled = jit.compile(deepcopy(model))
+    torch.jit.save(model_compiled, target_model_path.replace('.model', '.pt'))
 
 if __name__ == '__main__':
     main()
