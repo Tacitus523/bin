0f2528b2058530fbd6c9b7fe22710783d33d8e7e
Lukas Petersen <lukas.petersen@kit.edu>
2024-12-18 15:12:33 +0100

    fix: correct esp_gradients key to esp_gradient in train_mace.sh

--- mace_scripts/train_mace.sh
+++ mace_scripts/train_mace.sh
@@ -83,7 +83,7 @@ python /lustre/home/ka/ka_ipc/ka_he8978/MACE_QEq_development/mace-tools/scripts/
     --forces_key="ref_force" \
     --charges_key="ref_charge" \
     --esp_key="esp" \
-    --esp_gradients_key="esp_gradients" \
+    --esp_gradient_key="esp_gradients" \
     --formal_charges_from_data \
     --error_table="EFQRMSE" \
     --scale_atsize=1.0 \
