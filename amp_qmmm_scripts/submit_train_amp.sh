be67100a69a718e94ed8e620df5bd663a99015cb
Lukas Petersen <lukas.petersen@kit.edu>
2025-04-06 19:20:18 +0200

    feat: add submission script for training models with train_amp.py

---
+++ amp_qmmm_scripts/submit_train_amp.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Submission script for training a model using the train_amp.py script and submit_python_file_justus.sh script.
+
+print_usage() {
+    echo "Usage: $0 <config_file>"
+    echo "Submits a training job using the specified config file."
+    exit 1
+}
+
+config_file=$1
+if [ -z "$config_file" ]; then
+    echo "Error: No config file provided."
+    print_usage
+fi
+
+# check if the train_amp.py command is available
+if ! command -v train_amp.py &> /dev/null
+then
+    echo "train_amp.py could not be found. Please activate the appropiate environment."
+    exit 1
+fi
+
+submission_script=$(realpath "$(dirname "$(readlink -f "$0")")/../submit_python_file_justus.sh")
+amp_script=$(which train_amp.py)
+
+
+$submission_script -p $amp_script -c $config_file  
