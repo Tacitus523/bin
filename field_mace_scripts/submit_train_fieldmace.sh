58a869bdce3c24b4ed0f1e6f4faea4ca9c07c4cc
Lukas Petersen <lukas.petersen@kit.edu>
2025-04-28 15:44:16 +0200

    feat: add training and submission scripts for FieldMace model

---
+++ field_mace_scripts/submit_train_fieldmace.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+TRAIN_SCRIPT="train_fieldmace.sh"
+
+# Default data folder
+DATA_FOLDER="/lustre/work/ws/ws1/ka_he8978-dipeptide/training_data/B3LYP_aug-cc-pVTZ_water"
+#DATA_FOLDER="/lustre/work/ws/ws1/ka_he8978-thiol_disulfide/training_data/B3LYP_aug-cc-pVTZ_water"
+
+# Default number of epochs
+EPOCHS=100
+
+# Default number of submissions
+NUM_SUBMISSIONS=1
+
+# Default job name
+job_name=$(basename $PWD)
+
+print_usage() {
+    echo "Usage: $0 [-n number_of_submissions] [-e number_of_epochs] [-d data_folder]" >&2
+}
+
+# Parse arguments
+while getopts "n:e:d:" flag
+do
+    case $flag in
+        e) EPOCHS=${OPTARG};;
+        d) DATA_FOLDER=${OPTARG};;
+        n) NUM_SUBMISSIONS=$OPTARG ;;
+        *) print_usage; exit 1 ;;
+    esac
+done
+
+# Input validation
+if [ $NUM_SUBMISSIONS -lt 1 ]
+then
+    echo "Number of submissions must be at least 1" >&2
+    exit 1
+fi
+
+if [ $NUM_SUBMISSIONS -gt 10 ]
+then
+    echo "Number of submissions must be at most 10" >&2
+    exit 1
+fi
+
+# Submit jobs
+data_folder=$(readlink -f $DATA_FOLDER)
+echo "Data folder: $data_folder"
+echo "Number of epochs: $EPOCHS"
+echo "Number of submissions: $NUM_SUBMISSIONS"
+
+if [ $NUM_SUBMISSIONS -eq 1 ]
+then
+    sbatch --job-name=$job_name $TRAIN_SCRIPT -e $EPOCHS -d $data_folder
+    exit 0
+fi
+
+for ((i=0; i<NUM_SUBMISSIONS; i++))
+do
+    submission_dir="model_$i"
+    split_data_folder="$data_folder/split_$i"
+    mkdir -p $submission_dir
+    cd $submission_dir
+    sbatch --job-name="${job_name}_$i" $TRAIN_SCRIPT -e $EPOCHS -d $data_folder
+    cd ..
+done
---
+++ field_mace_scripts/train_fieldmace.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=32G
+#SBATCH --time=72:00:00
+#SBATCH --output=train.out
+#SBATCH --error=train.err
+#SBATCH --gres=gpu:1
+#SBATCH --mail-user=lukas.petersen@kit.edu
+#SBATCH --mail-type=END,FAIL
+
+module load compiler/gnu/10.2
+module load devel/cuda/12.3
+module load lib/cudnn/9.0.0_cuda-12.3
+
+# Relative paths in the data folder
+TRAIN_FILE="train.xyz"
+VALID_FILE="valid.xyz"
+TEST_FILE="test.xyz"
+# Default value, gets overwritten by the command line argument from submit_train_mace.sh
+EPOCHS=100
+
+MODEL_NAME="FieldMace"
+
+# Atomization energies for the DFT and DFTB methods, deprecated since config.yaml
+DFT_E0s='{1: -13.575035506869515, 6: -1029.6173622986487, 7: -1485.1410643783852, 8: -2042.617308911902, 16: -10832.265333248919}'
+DFTB_E0s='{1: -7.192493802609272, 6: -42.8033008522276, 7: -65.55164277599535, 8: -94.82677849249036}'
+
+
+if [ -z $SLURM_JOB_NAME ]; then
+    WANDB_NAME=$(basename $PWD)
+else
+    WANDB_NAME=$SLURM_JOB_NAME
+fi
+
+print_usage() {
+    echo "Usage: $0 [-e number_of_epochs] [-d data_folder]" >&2
+}
+
+# Parse command line arguments for epochs and data folder
+while getopts e:d: flag
+do
+    case "${flag}" in
+        e) EPOCHS=${OPTARG};;
+        d) DATA_FOLDER=${OPTARG};;
+        *) print_usage; exit 1;;
+    esac
+done
+
+data_folder=$(readlink -f $DATA_FOLDER)
+train_file="$data_folder/$TRAIN_FILE"
+valid_file="$data_folder/$VALID_FILE"
+test_file="$data_folder/$TEST_FILE"
+
+echo "Starting training: $(date)"
+echo "Data folder: $data_folder"
+echo "Train file: $train_file"
+echo "Valid file: $valid_file"
+echo "Test file: $test_file"
+echo "Number of epochs: $EPOCHS"
+
+# conda activate fieldmace
+mace_run_train \
+    --config config.yaml \
+    --name $MODEL_NAME \
+    --train_file $train_file \
+    --valid_file $valid_file \
+    --test_file $test_file \
+    --seed $RANDOM \
+    --max_num_epochs $EPOCHS \
+    --wandb_name $WANDB_NAME \
+    --num_workers 4 \
+
+training_exit_status=$?
+
+echo "Finished training: $(date)"
+
+# Convert the model to a scripted model
+if [ $training_exit_status -eq 0 ]
+then
+    convert_model_to_scripted_model.py --model_prefix $MODEL_NAME
+fi
