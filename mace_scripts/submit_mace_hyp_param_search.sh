0c07a4e11fe9e31f40f993bd360accffffd3b940
Lukas Petersen <lukas.petersen@kit.edu>
2025-10-02 19:21:35 +0200

    fix: update hyperparameter search script configuration and dataset size

--- mace_scripts/submit_mace_hyp_param_search.sh
+++ mace_scripts/submit_mace_hyp_param_search.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-STUDY_NAME="hyperband_search"
+#STUDY_NAME="num_cutoff_basis"
 MAX_TRIALS=1000
 MAX_EPOCHS_SEARCH=67
 HYPERBAND_FACTOR=3
-MAX_DATASET_SIZE=6000
+MAX_DATASET_SIZE=1000
 
-N_TASKS=4
+CONFIG_FILE="config.yaml"
+N_TASKS=2
 N_GPUS=2
 
 
@@ -21,11 +22,11 @@ wandb_name=$(basename $PWD)
 wandb_flag="--wandb_name $wandb_name"
 
 submit_python_file_justus.sh -e -j $N_TASKS -g $N_GPUS $HOME/MACE_QEq_development/mace-tools/scripts/lukas_hyperparameter_search.py -- \
-    --config config.yaml \
-    --study_name $STUDY_NAME \
+    --config $CONFIG_FILE \
     --max_trials $MAX_TRIALS \
     --max_epochs_search $MAX_EPOCHS_SEARCH \
     --max_train_size $MAX_DATASET_SIZE \
     --factor $HYPERBAND_FACTOR \
     --n_jobs $N_TASKS \
-    $wandb_flag
+    $wandb_flag #\
+    #--study_name $STUDY_NAME \
