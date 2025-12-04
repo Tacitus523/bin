f82124651c6cb9c28279e7777e4a82a4f2a4e5b7
Lukas Petersen <lukas.petersen@kit.edu>
2025-12-04 23:07:59 +0100

    fix: submit_multiple_walkers_justus scripts for GPU resource handling and argument validation

--- submit_multiple_walkers_justus.sh
+++ submit_multiple_walkers_justus.sh
@@ -2,7 +2,7 @@
 
 EMAIL=$MY_MAIL
 
-export N_WALKER=16 # Also has to be adjusted in plumed.dat
+export N_WALKER=16 # Will be read from plumed file if provided, default to 16 otherwise
 WALKER_SCRIPT="multiple_walkers_justus.sh"
 
 print_usage() {
@@ -10,8 +10,10 @@ print_usage() {
     exit 1
 }
 
+resource_flag="--gres=gpu:1"
+
 email_flag="--mail-user=$EMAIL --mail-type=END,FAIL"
-while getopts ":t:p:e" opt; do
+while getopts ":t:p:e:g:" opt; do
     case ${opt} in
         t )
             tpr_file=$OPTARG
@@ -21,6 +23,19 @@ while getopts ":t:p:e" opt; do
             ;;
         e )
             email_flag="--mail-user=$EMAIL --mail-type=END,FAIL"
+            ;;
+        g )
+            if [[ ! $OPTARG =~ ^[0-9]+$ ]]; then
+                echo "Invalid GPU count: $OPTARG"
+                print_usage
+                exit 1
+            fi
+            if [ $OPTARG -eq 0 ]; then
+                resource_flag=""
+            else
+                resource_flag="--gres=gpu:$OPTARG"
+            fi
+            echo "Requesting $OPTARG GPU(s) per node"
             ;;
         \? )
             echo "Invalid option: -$OPTARG" 1>&2
@@ -39,15 +54,23 @@ if [[ ! $tpr_file == *.tpr ]]; then
     echo "tpr file must end with .tpr. Got $tpr_file"
     print_usage
     exit 1
-fi
-echo "tpr_file: $tpr_file"
-if [ ! -f $plumed_file ]
-then
-    echo "plumed file not found. Got $plumed_file"
+elif [ ! -f "$tpr_file" ]; then
+    echo "tpr file not found. Got $tpr_file"
     print_usage
     exit 1
 fi
-echo "plumed_file: $plumed_file"
+echo "tpr_file: $tpr_file"
+
+if [ -z "$plumed_file" ]; then
+    echo "WARNING: Missing plumed file"
+elif [ ! -f "$plumed_file" ]; then
+    echo "plumed file not found. Got $plumed_file"
+    print_usage
+    exit 1
+else
+    export N_WALKER=$(grep "WALKERS_N=" $plumed_file | cut -d'=' -f2)
+    echo "plumed_file: $plumed_file"
+fi
 
 # Remaining arguments are additional files
 additional_files=("$@")
@@ -57,6 +80,5 @@ done
 
 job_name=$(basename $tpr_file .tpr)_metaD
 
-#N_TASKS=$((N_WALKER * 2)) # Run + observation
-N_TASKS=$N_WALKER # Only run, observation is lightweight
-sbatch $email_flag --ntasks-per-node=$N_TASKS --job-name=$job_name $WALKER_SCRIPT -t $tpr_file -p $plumed_file "${additional_files[@]}"
+N_TASKS=$N_WALKER
+sbatch $email_flag --ntasks-per-node=$N_TASKS --job-name=$job_name $resource_flag $WALKER_SCRIPT -t $tpr_file -p $plumed_file "${additional_files[@]}"
